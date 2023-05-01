60de5c36ba9fd36874fd63b56d5c89050c9f1560
NumberOneHero <wwwru98@live.com.pt>
2023-05-01 22:08:18 +0100

    kkk

--- StereoVision/Python/shape_recognition.py
+++ StereoVision/Python/shape_recognition.py
@@ -8,32 +8,27 @@ def find_circles(frame, mask):
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contours = imutils.grab_contours(contours)
-    centerf = None
-    centers = None
+    center = None
     fMaxArea = 0
     sMaxArea = 0
     fCnt = None
     sCnt = None
     # Only proceed if at least one contour was found
-    CntLen = len(contours)
-    if  CntLen > 0:
+    if len(contours) > 0:
         # Find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
         for cnt in contours:
             area = cv2.contourArea(cnt)
-
-
-            if area > fMaxArea:
-
+            if area > fMaxArea :
                 fMaxArea = area
                 fCnt = cnt
-            if area > sMaxArea and area < fMaxArea:
+            elif area > sMaxArea:
                 sMaxArea = area
                 sCnt = cnt
-            if CntLen == 1 :
-                sMaxArea = area
-                sCnt = cnt
+
+
+
 
         # c = max(contours, key=cv2.contourArea)
         ((xf, yf), radiusf) = cv2.minEnclosingCircle(fCnt)
@@ -44,8 +39,8 @@ def find_circles(frame, mask):
         radius = (radiuss + radiusf) / 2
 
 
-        Mf = cv2.moments(fCnt)
-        Ms = cv2.moments(sCnt)
+        Mf = cv2.moments(fMaxArea)
+        Ms = cv2.moments(sMaxArea)
 
 
 
@@ -53,16 +48,16 @@ def find_circles(frame, mask):
         # M = cv2.moments(c)       #Finds center point
         centerf = (int(Mf["m10"] / Mf["m00"]), int(Mf["m01"] / Mf["m00"]))
         centers = (int(Ms["m10"] / Ms["m00"]), int(Ms["m01"] / Ms["m00"]))
-        center = (int((centerf[0]+centers[0]) / 2 ),int((centerf[1]+ centers[1]) / 2))
-        print(center)
+
+        print(centerf)
         # Only proceed if the radius is greater than a minimum value
         if radiusf > 10:
             # Draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(frame, (int(x), int(y)), int(radius),
             	(0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 0), -1)
-            cv2.drawContours(frame, fCnt, -1, (0, 255, 0), 3)
-            cv2.drawContours(frame, sCnt, -1, (255, 0, 0), 3)
+            cv2.circle(frame, centerf, 5, (0, 0, 0), -1)
+            cv2.drawContours(frame, fMaxArea, -1, (255, 0, 0), 3)
+            cv2.drawContours(frame, sMaxArea, -1, (255, 0, 0), 3)
 
-    return center
+    return centerf
